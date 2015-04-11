4a03e3fa2ec8b496c82a446eabe60c0de6853c57
diego <diego8525@gmail.com>
2015-04-10 21:24:15 -0500

    contenidos

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,4 +2,17 @@
 
 <h1>Institucion Educativa Federico Ozanam (<i><?php echo CHtml::encode(Yii::app()->name); ?></i>)	</h1>
 
-<p class="lead">Aula virtual para estudientes, manejo de notas, usuarios, docentes y clases tanto presenciales como virtuales.</p>
+<p class="lead">Aula virtual para estudiantes, manejo de notas, usuarios, docentes y clases tanto presenciales como virtuales.</p>
+
+
+<div class="media well">
+  <a class="pull-left" href="#">
+    <img class="media-object img-circle" src="<?php echo Yii::app()->baseUrl.'/themes/OzAuLink/images/curso.png'; ?>" style='background-color:#D7D8D6;'>
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Curso</h4>
+    <div class="media">
+      asdsadasdasdsad
+    </div>
+  </div>
+</div>
