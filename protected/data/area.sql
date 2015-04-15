644c06a92eda71d0ff40664c3f45dd103cedaa1d
oscar mesa <oscarmesa.elpoli@gmail.com>
2015-04-15 12:39:30 -0500

    cambios de datos

--- protected/data/area.sql
+++ protected/data/area.sql
@@ -1,54 +1,6 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : local
-Source Server Version : 50616
-Source Host           : localhost:3306
-Source Database       : math
-
-Target Server Type    : MYSQL
-Target Server Version : 50616
-File Encoding         : 65001
-
-Date: 2015-03-26 21:17:52
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `area`
--- ----------------------------
-DROP TABLE IF EXISTS `area`;
-CREATE TABLE `area` (
-`id_area`  int(5) NOT NULL AUTO_INCREMENT ,
-`descripcion`  varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-PRIMARY KEY (`id_area`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=7
-
-;
-
--- ----------------------------
--- Records of area
--- ----------------------------
-BEGIN;
-INSERT INTO `area` VALUES ('1', 'Ciencias Naturales'), ('2', 'Ciencias Sociales'), ('3', 'Educación Artística'), ('4', 'Humanidades'), ('5', 'Matemáticas'), ('6', 'Tegnología e Informática');
-COMMIT;
-
--- ----------------------------
--- Auto increment value for `area`
--- ----------------------------
-ALTER TABLE `area` AUTO_INCREMENT=7;
-
-
-
-ALTER TABLE `materias`
-MODIFY COLUMN `nombre_materia`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL AFTER `idmaterias`,
-MODIFY COLUMN `state_materia`  enum('active','inactive') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL AFTER `nombre_materia`,
-ADD COLUMN `idarea`  int(5) NOT NULL AFTER `state_materia`;
-
-
-ALTER TABLE `materias` ADD CONSTRAINT `fk_id_area_asignatura` FOREIGN KEY (`idarea`) REFERENCES `area` (`id_area`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
+INSERT INTO `area` (`id_area`, `descripcion`, `idestado`) VALUES (1, 'Ciencias Naturales 1', 1);
+INSERT INTO `area` (`id_area`, `descripcion`, `idestado`) VALUES (2, 'Ciencias Sociales', 1);
+INSERT INTO `area` (`id_area`, `descripcion`, `idestado`) VALUES (3, 'Educación Artística', 1);
+INSERT INTO `area` (`id_area`, `descripcion`, `idestado`) VALUES (4, 'Humanidades', 1);
+INSERT INTO `area` (`id_area`, `descripcion`, `idestado`) VALUES (5, 'Matemáticas', 1);
+INSERT INTO `area` (`id_area`, `descripcion`, `idestado`) VALUES (6, 'Tegnología e Informática', 1);
