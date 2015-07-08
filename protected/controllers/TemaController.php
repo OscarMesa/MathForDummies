38548fe5207d391f122923b4bf7704d8975240ab
oscar mesa <oscarmesa.elpoli@gmail.com>
2015-07-08 03:25:35 -0500

    cambios

--- protected/controllers/TemaController.php
+++ protected/controllers/TemaController.php
@@ -38,6 +38,7 @@ class TemaController extends Controller {
      * @param integer $id the ID of the model to be displayed
      */
     public function actionView($id) {
+        echo 'esete es '.$id;die;
         $this->layout ="modal";
         $this->render('view', array(
             'model' => $this->loadModel($id),
