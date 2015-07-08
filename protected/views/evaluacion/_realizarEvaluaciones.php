cc746758800a20cc8bc7aa8993aff019a98dd2a1
oscar mesa <oscarmesa.elpoli@gmail.com>
2015-07-08 07:08:48 -0500

    cabios

--- protected/views/evaluacion/_realizarEvaluaciones.php
+++ protected/views/evaluacion/_realizarEvaluaciones.php
@@ -1,7 +1,7 @@
 <div class="content-examen" style="">
     <div class="content-img-examen">
     <a href="<?php echo Yii::app()->createAbsoluteUrl('evaluacionIntegrante/create',array('id_evaluacion'=>$data->id_evaluacion));?>" class="img-evalucion">
-        <img src="<?php echo Yii::app()->createAbsoluteUrl('themes/OzAuLink/images/evaluacion.png')?>"/>
+        <img src="<?php echo Yii::app()->getBaseUrl(); ?>/themes/OzAuLink/images/evaluacion.png"/>
     </a>
     </div><br/>
     <span class="content-description">
