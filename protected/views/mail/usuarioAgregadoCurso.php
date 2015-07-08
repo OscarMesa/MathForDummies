22e0845f2cc2e6b2610613bdcff40651c551a02a
oscar mesa <oscarmesa.elpoli@gmail.com>
2015-07-08 04:43:23 -0500

    cambios

--- protected/views/mail/usuarioAgregadoCurso.php
+++ protected/views/mail/usuarioAgregadoCurso.php
@@ -98,7 +98,7 @@ html
       <tr>
           <td>
               <p><span>Diríjase al siguiente link para darle un vistazo al curso.</span></p> <br/>
-              <a href="<?php echo Yii::app()->createAbsoluteUrl('cursos/index',array($curso->id)); ?>">Click aqui</a>
+              <a href="<?php echo Yii::app()->createAbsoluteUrl('cursos',array('id'=>$curso->id)); ?>">Click aqui</a>
           </td>
       </tr>
 </table>
